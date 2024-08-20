73cde98a2a87a98b8b1957b81c20f24585032cd8
Mayank Mishra <mayank71104@gmail.com>
2024-08-20 20:05:13 +0530

    error

--- mainapp/views.py
+++ mainapp/views.py
@@ -65,7 +65,7 @@ def mappage(request):
                         "Walk": 0,
                         "Bike": 74,
                         "Ebike": 22,
-                        "Rickshaw": 25.67,
+                        "Rickshaw": 25.
                         "Erickshaw": 13.33,
                         "Scooter": 55,
                         "E-scooter": 22
